1ec53a7ab45986e8034d80508c48130ed8fcdbf5
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-02-28 19:46:21 -0600

    mvp 500 rooms 998 moves

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Stack, Queue
 
 import random
 from ast import literal_eval
@@ -24,12 +25,65 @@ world.load_graph(room_graph)
 world.print_rooms()
 
 player = Player(world.starting_room)
-
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
 traversal_path = []
+visited = {}
 
+def return_opposites(direction):
+    if direction == "n":
+        return "s"
+    if direction == "s":
+        return "n"
+    if direction == "e":
+        return "w"
+    if direction == "w":
+        return "e"
+def move(direction):
+    player.travel(direction)
+    traversal_path.append(direction)
+    
+def add_to_visited(current_room_id, exits):
+    visited[current_room_id] = {}
+    for e in exits:
+        visited[current_room_id][e] = None        
+    
 
+def dft_maze_traversal(current_room):
+    current_room = player.current_room.id
+    current_exits = player.current_room.get_exits()
+    prev_room = None
+    s = Stack()
+
+    s.push([None, current_room, prev_room, current_exits])
+    while len(visited) < 499:
+        curr_node = s.pop()
+        direction = curr_node[0]
+        current_room = curr_node[1]
+        prev_room = curr_node[2]
+        curr_exits = curr_node[3]
+        if current_room not in visited:
+            add_to_visited(current_room, curr_exits)
+        if direction is not None:
+            visited[current_room][return_opposites(direction)] = prev_room
+        if prev_room is not None:
+            visited[prev_room][direction] = current_room
+        for d in visited[current_room].keys():
+           
+            if visited[current_room][d] is None:
+                s.push(curr_node)
+                prev = player.current_room.id                
+                move(d)
+                s.push([d, player.current_room.id, prev, player.current_room.get_exits()])
+                break
+        if current_room == player.current_room.id:
+            move(return_opposites(direction))
+                      
+dft_maze_traversal(player.current_room.id)
+                
+                # Get id
+                # Get exits, previous room, current room, 
+                # Update visited
+                # 
+                
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -45,8 +99,6 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
-
 
 #######
 # UNCOMMENT TO WALK AROUND
